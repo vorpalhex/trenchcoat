7632a682134afd746efc5c6d25dcb6a194a0c15b
sharpshark28 <sharpshark28@gmail.com>
2017-08-26 13:58:57 -0500

    [Scaffolding]: react compile to client/app

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'client/index.html'),
+    pathname: path.join(__dirname, 'client/app/index.html'),
     protocol: 'file:',
     slashes: true
   }));
