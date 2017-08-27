5e59aad87fbffa9f44e25c5392f58bf10ba05ca6
Derrick Hinkle <vorpalhex@users.noreply.github.com>
2017-08-27 13:57:52 -0500

    Break out models and get encryption working

--- client/journal/index.js
+++ client/journal/index.js
@@ -1,66 +1,16 @@
+'use strict';
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
-const crypt = require('crypto'); //yes we're using a weird name for this because global scope conflicts
 
 const journalPath = path.join(os.homedir(), './primary.jrnl');
 const errors = require('./errors');
-const JournalModel = require('./model');
+const JournalModel = require('./models/journal');
+const JournalWrapper = require('./models/journalWrapper');
 
 let currentWrapper = null;
 let currentJournal = null;
 let currentPassword = null;
-
-class JournalWrapper {
-  constructor(wrapperObject={}) {
-    if(!wrapperObject.version || wrapperObject.version !== '1.0') {
-      throw new errors.UnsupportedJournalError();
-    }
-
-    if(!wrapperObject.payload) {
-      throw new errors.CorruptedJournalError('Missing payload');
-    }
-
-    this.version = wrapperObject.version;
-    this.payload = wrapperObject.payload;
-    this.algorithm = 'aes-256-cbc'; //this is always hardcoded.
-  }
-
-  getKeyAndIV(password, cb) {
-    const saltKey = 'notsecure'; //generate these on install somehow?
-    const saltIV = 'notsecure';
-    crypt.pbkdf2(password, saltIV, 10000, 512, 'sha512', (err, IV) => {
-      if(err) return cb(err);
-      crypt.pbkdf2(password, saltKey, 10000, 512, 'sha512', (err, key) => {
-        if(err) return cb(err);
-        return cb(null, {key, IV});
-      });
-    });
-  }
-
-  decrypt(password, cb) {
-    this.getKeyAndIV(password, (err, res) => {
-      if(err) return cb(err);
-      const cipher = crypt.createDecipheriv(this.algorithm, res.key, res.iv);
-      const result = cipher.update(this.payload, 'utf8', 'binary');
-      result += cipher.final('binary');
-
-      return cb(null, result);
-    });
-  }
-
-  encrypt(raw, password, cb) {
-    this.getKeyAndIV(password, (err, res) => {
-      if(err) return cb(err);
-      const cipher = crypt.createCipheriv(this.algorithm, res.key, res.iv);
-      const result = cipher.update(raw, 'utf8', 'binary');
-      result += cipher.final('binary');
-
-      this.payload = result;
-      return cb();
-    });
-  }
-}
 
 function loadJournal(password, cb) {
   const jrnlWrapperString = fs.readFileSync(journalPath, {encoding: 'utf8'});
@@ -87,7 +37,7 @@ function saveJournal(cb) {
   if(!currentWrapper) {
     currentWrapper = new JournalWrapper({
       version: '1.0',
-      payload: ''
+      payload: '{}'
     });
   }
 
@@ -101,7 +51,7 @@ function saveJournal(cb) {
 
     return fs.writeFile(journalPath, jrnlWrapperString, {encoding: 'utf8'}, (err) => {
       if(err) return cb(err);
-      return cb(null, err);
+      return cb(null, currentJournal);
     });
   });
 }
@@ -110,12 +60,13 @@ function newJournal(password) {
   currentPassword = password;
   currentJournal = new JournalModel();
 
-  return true;
+  return currentJournal;
 }
 
 
 module.exports = {
   newJournal,
   loadJournal,
-  saveJournal
+  saveJournal,
+  currentJournal
 };
--- client/journal/models/journalEntry.js
+++ client/journal/models/journalEntry.js
@@ -1,8 +1,8 @@
 const uuid = require('uuid');
 const showdown = require('showdown');
-const markdownConverter = new showdown.Converter(),
+const markdownConverter = new showdown.Converter();
 
-class JournalEntry {
+module.exports = class JournalEntry {
   constructor(entryObject={}) {
     this.id = entryObject.id || uuid.v4();
     this._title = entryObject.title || 'Unnamed';
@@ -28,29 +28,5 @@ class JournalEntry {
     this.body.html = markdownConverter.makeHtml(markdown);
     this.body.markdown = markdown;
     this.updated_at = Date.now();
-  }
-}
-
-module.exports = class Journal {
-  constructor(journalObject={}) {
-    this.entries = [];
-    this.version = '1.0';
-
-    const entries = ( journalObject.entries && journalObject.entries.slice() ) || [];
-    entries.forEach( this.addEntry );
-  }
-  addEntry(entry) {
-    this.entries.push( new JournalEntry( entry ) );
-  }
-  getEntry(id) {
-    results = this.entries.filter( (entry) => entry.id === id);
-    return results[0];
-  }
-  sortEntries(field='created_at', fn) {
-    if(!fn) {
-      fn = (a, b) => b[field] - a[field];
-    }
-
-    this.entries.sort( fn );
   }
 }
