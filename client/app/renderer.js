da777b18a9e6dee6d77fcc5189da27b214d34a7e
sharpshark28 <sharpshark28@gmail.com>
2017-08-26 22:53:13 -0500

    [Unlocker]: Password length checker

--- client/app/renderer.js
+++ client/app/renderer.js
@@ -22204,17 +22204,37 @@ class App extends __WEBPACK_IMPORTED_MODULE_0_react__["Component"] {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
 
 
+const passwordRankings = [{ length: 0, quality: 0, text: "Too short." }, { length: 12, quality: 1, text: "Could be better." }, { length: 22, quality: 2, text: "Getting there..." }, { length: 32, quality: 3, text: "Great! You're secure." }];
+const getPasswordRanking = password => passwordRankings.reduce((previous, current) => password >= current.length ? current : previous);
+
 class Unlock extends __WEBPACK_IMPORTED_MODULE_0_react__["Component"] {
+  constructor() {
+    super();
+    this.state = {
+      passwordQuality: -1,
+      passwordHelp: ""
+    };
+  }
+
   attemptUnlock(event) {
     event.preventDefault();
     this.props.attemptUnlock(this.password.value);
+  }
+
+  rankPassword() {
+    const ranking = getPasswordRanking(this.password.value.length);
+    this.setState({
+      passwordQuality: ranking.quality,
+      passwordHelp: ranking.text
+    });
   }
 
   render() {
     const { status } = this.props;
     const loading = status === "loading";
     const loaded = status === "loaded";
-    const disable = loading || loaded;
+    const disableInput = loading || loaded;
+    const disableSubmit = loading || loaded || this.state.passwordQuality <= 0;
     let formClass = "unlock";
     if (loaded) formClass += " animated slideOutRight";
     return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
@@ -22230,13 +22250,18 @@ class Unlock extends __WEBPACK_IMPORTED_MODULE_0_react__["Component"] {
         ),
         __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
           "label",
-          null,
-          "Password",
-          __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("input", { type: "password", ref: input => this.password = input, disabled: disable, autoFocus: true })
+          { className: "quality-" + this.state.passwordQuality },
+          "Password ",
+          __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+            "em",
+            null,
+            this.state.passwordHelp
+          ),
+          __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("input", { type: "password", ref: input => this.password = input, disabled: disableInput, onChange: e => this.rankPassword(e), autoFocus: true })
         ),
         __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
           "button",
-          { type: "submit", disabled: disable },
+          { type: "submit", disabled: disableSubmit },
           loading ? "Loading..." : "Unlock"
         )
       )
